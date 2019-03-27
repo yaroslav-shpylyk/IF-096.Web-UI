97086c239c0f922077508d1afa06324b86b692cd
ya_ron <dr.likle@gmail.com>
2019-03-28 00:22:38 +0200

    Added token interceptor

---
+++ src/app/shared/services/token-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+
+@Injectable()
+export class TokenInterceptorService implements HttpInterceptor {
+
+  constructor(private authService: AuthService) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    const token = this.authService.getToken();
+    request = request.clone({
+      setHeaders: {
+        Authorization: token
+      }
+    });
+    return next.handle(request)
+      .pipe(tap( _ => {}, error => {
+        // do something when token is not valid
+      }));
+  }
+}
