e8246c9b39d27e57f7feaf2708a6f8912afab62e
ya_ron <dr.likle@gmail.com>
2019-03-29 00:14:00 +0200

    Fixed login module structure

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators} from '@angular/forms';
-import { AuthService } from '../../shared/services/auth.service';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
