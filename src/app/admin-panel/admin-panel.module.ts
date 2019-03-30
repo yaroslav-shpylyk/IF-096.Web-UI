b0b2a6a38b1e77882253ae25080adac5c90080c4
Yaroslav Virstiuk <yarovji@gmail.com>
2019-03-30 16:43:22 +0200

    g comp. groups

--- src/app/admin-panel/admin-panel.module.ts
+++ src/app/admin-panel/admin-panel.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { GroupsComponent } from './admin-panel/groups/groups.component';
 
 @NgModule({
-  declarations: [AdminPanelComponent],
+  declarations: [AdminPanelComponent, GroupsComponent],
   imports: [
     CommonModule,
     AdminPanelRoutingModule
