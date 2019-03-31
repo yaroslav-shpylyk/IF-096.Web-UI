9c658ef2d0b58745ddefd87e0745a0ef94f86a82
denys <den.hodovanets@gmail.com>
2019-03-31 11:11:49 +0300

    add enum and fixed code

---
+++ src/app/enum/roles.enum.ts
@@ -0,0 +1,5 @@
+export enum roles {
+    'ROLE_TEACHER',
+    'ROLE_USER',
+    'ROLE_ADMIN'
+}
--- src/app/services/shell.guard.ts
+++ src/app/services/shell.guard.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router } from "@angular/router";
-import { AuthService } from "./auth.service"
+import { AuthService } from "./auth.service";
+import { roles } from "../enum/roles.enum";
 
 
 @Injectable({
@@ -8,13 +9,23 @@ import { AuthService } from "./auth.service"
 })
 export class ShellGuard implements CanActivate {
 
-  roles: Array<string> = ['ROLE_TEACHER', 'ROLE_USER', 'ROLE_ADMIN'];
+  roles: Array<string> = [roles[0], roles[1], roles[2]];
 
   constructor(public auth: AuthService, private router: Router) {
   }
+  
+  /**
+  * Method check are you logged. If you are logged, method check roles from 
+  * token and  you will enter to shell component, if not,
+  * you will be redirect to login component
+  * @returns - if you have logged return true, else false
+  */
 
   canActivate(): boolean {
-    if (this.roles.some(role => role === this.auth.getUserRole())) {
+
+    let checkRoleInToken: string | boolean = this.auth.getUserRole();
+    let findcheckedRoleInEnum: boolean = this.roles.some(role => role === checkRoleInToken);
+    if (findcheckedRoleInEnum) {
       return true
     }
     else {
