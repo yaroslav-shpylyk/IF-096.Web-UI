a72a7206cb21ef3af1e4fd911232d082b72dba8c
trChris <christinagr123123@gmail.com>
2019-04-01 11:09:12 +0300

    Added Angular Material and Angular Flex-Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { TokenInterceptorService } from './services/token-interceptor.service';
@@ -6,15 +5,19 @@ import { AppComponent } from './app.component';
 import {AppRoutingModule} from './app-routing.module';
 import { AuthService } from './services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FlexLayoutModule} from '@angular/flex-layout';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    FlexLayoutModule
   ],
   providers: [
     AuthService,
