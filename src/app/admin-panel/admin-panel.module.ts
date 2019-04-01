ef6d22313d6e0ce8979b4d9c37d25fa102fb844e
Yaroslav Virstiuk <yarovji@gmail.com>
2019-04-01 19:41:24 +0300

    some merge

--- src/app/admin-panel/admin-panel.module.ts
+++ src/app/admin-panel/admin-panel.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AdminPanelRoutingModule } from './admin-panel-routing.module';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GroupsComponent } from './admin-panel/groups/groups.component';
 
 @NgModule({
