a257f685dd049f3e1b9898fb00d06a5c1d4ecc49
Yaroslav Virstiuk <yarovji@gmail.com>
2019-04-01 12:26:34 +0300

    add second get request if after was put request

--- src/app/admin-panel/admin-panel/groups/groups.component.ts
+++ src/app/admin-panel/admin-panel/groups/groups.component.ts
@@ -1,13 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-
 import { Group } from '../../../models/group-data.model';
-import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
 import { GroupsService } from 'src/app/services/groups.service';
 import { FormControl } from '@angular/forms';
-import { stringify } from '@angular/core/src/render3/util';
-
 
 
 
@@ -18,13 +13,13 @@ import { stringify } from '@angular/core/src/render3/util';
 })
 export class GroupsComponent implements OnInit {
   
+  
   groups: Group[];
   toggleForAcniveGroups: boolean = false;
   toggleForNotAcniveGroups: boolean = false;
   toggleForAddGroups: boolean = false;
   toggleForRechangeGroups: boolean = false;
   dataСhosenRechange:any;
-  dataСhosenRechange2 = 'fdf';
   nameChange:any;
   yearChange:any;
   infaChange:any;
@@ -42,34 +37,28 @@ export class GroupsComponent implements OnInit {
 
   addGrup(fields: Object) {
     const group = new Group(fields);
-    this.groupServices.addGrup(group).subscribe(
-      group => this.groups.push(group)
-    );
+    if (Number(group.id)){
+      this.groupServices.addGrup(group).subscribe(
+        _ => this.refreshGroups()
+      )
+    }else {
+      this.groupServices.addGrup(group).subscribe(
+        group => this.groups.push(group)
+      );
+    }
   }
 
   changeGroup(groupCh:any){
     this.toggleForRechangeGroups = !this.toggleForRechangeGroups
-    if(this.toggleForAcniveGroups){
-      this.toggleForAcniveGroups = !this.toggleForAcniveGroups
-    }
-    if (this.toggleForNotAcniveGroups){
-      this.toggleForNotAcniveGroups = !this.toggleForNotAcniveGroups
-    }
+    
     this.dataСhosenRechange = groupCh;
-
     this.nameChange = new FormControl('');
     this.nameChange.setValue(this.dataСhosenRechange.className);
-
     this.yearChange = new FormControl('');
     this.yearChange.setValue(this.dataСhosenRechange.classYear);
-
     this.infaChange = new FormControl('');
     this.infaChange.setValue(this.dataСhosenRechange.classDescription);
-
     this.idChange = new FormControl('');
     this.idChange.setValue(this.dataСhosenRechange.id);
-
-    console.log(typeof this.dataСhosenRechange);
-    console.log(this.dataСhosenRechange);
   }
 }
