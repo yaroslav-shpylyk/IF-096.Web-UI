11d17a60fdbd930adf7ee363e3c77dca1e00426e
Mykola Trots <trots86@gmail.com>
2019-04-02 17:45:05 +0300

    Removed hammerjs, fixed indentations

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login/login.component';
 import { ReactiveFormsModule } from '@angular/forms';
-import {MaterialModule} from '../material.module';
+import { MaterialModule } from '../material.module';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,7 @@
 import { NgModule } from '@angular/core';
-import {FlexLayoutModule} from '@angular/flex-layout';
-import {LayoutModule} from '@angular/cdk/layout';
-import {
-  MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule
-} from '@angular/material';
-
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule } from '@angular/material';
 
 @NgModule({
   imports: [
