5594a0bd61e7f5e6466033f1fe18264b770184b6
ya_ron <dr.likle@gmail.com>
2019-04-04 23:57:40 +0300

    Added router outlet to shell

--- src/app/admin-panel/admin-panel-routing.module.ts
+++ src/app/admin-panel/admin-panel-routing.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ShellComponent } from '../shell/shell/shell.component';
 import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { TeacherEditComponent } from './teachers/teacher-edit/teacher-edit.component';
 import { DialogEntryComponent } from './teachers/teachers-list/dialog/dialog-overview';
 import { TeachersListComponent } from './teachers/teachers-list/teachers-list.component';
 import { StudentsListComponent } from './students-list/students-list.component';
- 
+
 const routes: Routes = [
   {
     path: '',
@@ -16,7 +15,10 @@ const routes: Routes = [
     path: 'students',
     component: StudentsListComponent
   },
-  { path: 'teachers/new', component: TeacherEditComponent },
+  {
+    path: 'teachers/new',
+    component: TeacherEditComponent
+  },
   {
     path: 'teachers',
     component: TeachersListComponent,
@@ -27,8 +29,10 @@ const routes: Routes = [
       }
     ]
   },
-  { path: 'teachers/:id/edit', component: TeacherEditComponent }
-
+  {
+    path: 'teachers/:id/edit',
+    component: TeacherEditComponent
+  }
 ];
 
 @NgModule({
