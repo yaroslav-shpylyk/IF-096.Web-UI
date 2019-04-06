a8977a48b25d065582a518d8f969fe4b2ed4e91c
trChris <christinagr123123@gmail.com>
2019-04-07 00:02:35 +0300

    Create an Observable from scroll event

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,41 +1,44 @@
-import {Component, OnInit} from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { fromEvent, interval } from 'rxjs';
+import { debounce } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements AfterViewInit {
 
-  ngOnInit() {
-
-    window.onload = (() => {
-      header.classList.add('notransition');
-      header.classList.remove('hide');
-    }); // always show header after reloading page on any scroll height
-
+  ngAfterViewInit() {
     const header = document.getElementById('header');
-    let isScrolling: number;
 
-    window.addEventListener('scroll', (event) => {
-      header.classList.add('hide');
+    const isScrolling = fromEvent(window, 'scroll')
+      .subscribe(() => {
+        let doc = document.documentElement;
+        doc = (doc.clientHeight) ? doc : document.body;
+
+        if (doc.scrollTop >= 0 && doc.scrollTop <= 50) {
+          header.classList.add('notransition');
+          header.classList.remove('hide'); // add header without delay when user scroll to top of the page
+        } else {
+          header.classList.add('hide');
+          header.classList.remove('notransition'); // run when user is scrolling
+          console.log('fi');
+        }
+      });
+
+    const stoppedScrolling = fromEvent(window, 'scroll').pipe(
+      debounce(() => interval(2000))
+    ).subscribe(() => {
+      header.classList.remove('hide');
       header.classList.remove('notransition');
-      window.clearTimeout(isScrolling);
-      // Set a timeout to run after scrolling ends
-      isScrolling = setTimeout(() => {
-        header.classList.remove('hide');
-        header.classList.remove('notransition');
-      }, 2000);
-    }, false); // Listen for scroll events
+      console.log('se');
+    }); // run after scroll is finished with 1.5s delay
 
-    window.addEventListener('scroll', (event) => {
-      const body = document.body;
-      let doc = document.documentElement;
-      doc = (doc.clientHeight) ? doc : body;
-      if (doc.scrollTop >= 0 && doc.scrollTop <= 50) {
+    const reloading = fromEvent(window, 'load')
+      .subscribe(() => {
         header.classList.add('notransition');
         header.classList.remove('hide');
-      } // Listen for scroll events when user scroll to top of the page
-    });
+      }); // always show header after reloading page on any scroll height
   }
 }
