4fd8c0465472fedc3468021b23b0d2924ea4ccfd
Yarovji <39587257+Yarovji@users.noreply.github.com>
2019-04-06 19:17:56 +0300

    Update group-data.model.ts

--- src/app/models/group-data.model.ts
+++ src/app/models/group-data.model.ts
@@ -1,11 +1,11 @@
 export class Group {
-    id: number;
-    classYear: number;
-    className: string;
-    classDescription: string;
-    isActive: boolean;
-    numOfStudents: number;
-    constructor(fields:Object){
-       Object.assign(this, fields)
-     }
+  id: number;
+  classYear: number;
+  className: string;
+  classDescription: string;
+  isActive: boolean;
+  numOfStudents: number;
+  constructor(fields:Object){
+    Object.assign(this, fields)
   }
+}
