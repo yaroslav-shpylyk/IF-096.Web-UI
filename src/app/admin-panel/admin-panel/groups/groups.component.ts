946b21a7be0652cf0a5c34a737e76863c9aba335
Yarovji <39587257+Yarovji@users.noreply.github.com>
2019-04-06 20:04:35 +0300

    Update groups.component.ts

--- src/app/admin-panel/admin-panel/groups/groups.component.ts
+++ src/app/admin-panel/admin-panel/groups/groups.component.ts
@@ -12,34 +12,33 @@ import {MatBottomSheet, MatTableDataSource, MatSort} from '@angular/material';
 })
 
 export class GroupsComponent implements OnInit {
-
   @ViewChild(MatSort) sort: MatSort;
   groups: Group[];
   displayedColumns: string[] = ['className', 'classYear', 'isActive', 'id'];
   dataSource: MatTableDataSource<Group>;
- 
-  constructor(private groupServices: GroupsService,
-              private bottomSheet: MatBottomSheet) { }
-           
-    ngOnInit() {
-      this.refreshGroups()
-    }
 
-   /**
-   * Method open bottom sheet, send data to the bottom sheet,
-   *  updates list of class after closing bottom sheet
-   */
-    openBottomSheet(element:Object) {
-      let sheet = this.bottomSheet.open(AddModifyComponent,{
-        hasBackdrop: true,
-        data: (element) ? element : Group   
-      });
-      sheet.afterDismissed().subscribe(()=> this.refreshGroups())
-    }
+  constructor(private groupServices: GroupsService,
+  private bottomSheet: MatBottomSheet) { }
+           
+  ngOnInit() {
+    this.refreshGroups()
+  }
+
+  /**
+  * Method open bottom sheet, send data to the bottom sheet,
+  *  updates list of class after closing bottom sheet
+  */
+  openBottomSheet(element:Object) {
+    let sheet = this.bottomSheet.open(AddModifyComponent,{
+      hasBackdrop: true,
+      data: (element) ? element : Group   
+    });
+    sheet.afterDismissed().subscribe(()=> this.refreshGroups())
+  }
     
-   /**
-   * Method updates list of class and creates the possibility of sorting them
-   */
+  /**
+  * Method updates list of class and creates the possibility of sorting them
+  */
   refreshGroups(){
     this.groupServices.getGroups().subscribe( data =>  {
       this.groups = data;
@@ -47,5 +46,4 @@ export class GroupsComponent implements OnInit {
       this.dataSource.sort = this.sort;
     });
   }
-
 }
