cd71f88caa1756060d87343c367a5895297017f2
Yarovji <39587257+Yarovji@users.noreply.github.com>
2019-04-08 11:05:48 +0300

    Update admin-panel.module.ts

--- src/app/admin-panel/admin-panel.module.ts
+++ src/app/admin-panel/admin-panel.module.ts
@@ -12,16 +12,16 @@ import { StudentsListComponent } from './students-list/students-list.component';
 import { PlatformModule } from '@angular/cdk/platform';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GroupsComponent } from './admin-panel/groups/groups.component';
-import {MatRadioModule} from '@angular/material/radio';
-import {MatFormFieldModule} from '@angular/material/form-field';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { AddModifyComponent } from './admin-panel/groups/add-modify/add-modify.component';
-import {MatInputModule, MatDialogModule, MatIconModule, MatSelectModule} from '@angular/material';
-import {MatTableModule} from '@angular/material/table';
-import {MatButtonModule} from '@angular/material/button';
-import {MatExpansionModule} from '@angular/material/expansion';
-import {MatBottomSheetModule} from '@angular/material/bottom-sheet';
-import {MatListModule} from '@angular/material/list';
-import {MatSortModule} from '@angular/material/sort';
+import { MatInputModule, MatDialogModule, MatIconModule, MatSelectModule } from '@angular/material';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatBottomSheetModule } from '@angular/material/bottom-sheet';
+import { MatListModule } from '@angular/material/list';
+import { MatSortModule } from '@angular/material/sort';
 
 @NgModule({
   declarations: [
