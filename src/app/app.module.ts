7fc372aced0785a2f639ea95f9511a43f9443cee
Tetiana Mysiuk <tivasyuk@i.ua>
2019-04-09 01:23:23 +0300

    add locale

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,8 @@ import { TeachersService } from './admin-panel/teachers/teachers.service';
 import { DialogOverviewExampleDialog } from './admin-panel/teachers/teachers-list/dialog/dialog-overview';
 import { TeacherService } from './services/teacher.service';
 import { SubjectService } from './services/subject.service';
+//import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
 
 @NgModule({
@@ -38,7 +40,10 @@ import { SubjectService } from './services/subject.service';
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptorService,
       multi: true
-    }
+    },
+    {provide: MAT_DATE_LOCALE, useValue: 'ua-UA'}
+    //{provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    //{provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
   ],
   bootstrap: [AppComponent]
 })
