7be6a7447cf2c84dcac01a4ea773a9d943aa8d37
ya_ron <dr.likle@gmail.com>
2019-04-11 10:33:03 +0300

    Removed student progress methods from mark service

--- src/app/services/mark.service.ts
+++ src/app/services/mark.service.ts
@@ -1,11 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import { ClassService} from './class.service';
-import { StudentsService } from './students.service';
 import { MarksRequestOptions } from '../models/marks-request-options';
-import { map, switchMap, tap} from 'rxjs/operators';
-import { combineLatest } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { MarkData } from '../models/mark-data';
 
 @Injectable({
@@ -23,10 +20,10 @@ export class MarkService {
     let requestParams;
     if (options !== undefined && Object.keys(options).length) {
       const optionKeys = Object.keys(options);
-      const formatedOptions = optionKeys
+      const formattedOptions = optionKeys
         .map(item => `${item}=${options[item]}`)
         .join('&');
-      requestParams = `?${formatedOptions}`;
+      requestParams = `?${formattedOptions}`;
     } else {
       requestParams = '';
     }
@@ -34,43 +31,5 @@ export class MarkService {
       .pipe(
         map((result: {status: any, data: MarkData[]}) => result.data)
       );
-  }
-  public getProgressMarks(options?: MarksRequestOptions): Observable<any> {
-    const uniqueDates = [];
-    return options.student_id.length === 1 ?
-      this.getMarks(options) :
-      combineLatest(options.student_id.map(item => {
-        options.student_id = [item];
-        return this.getMarks(options);
-      }))
-        .pipe(
-          tap(result => {
-            const dates = [];
-            for (const student of result) {
-              for ( const mark of student ) {
-                const date =  new Date(mark.x[0], mark.x[1], mark.x[2]);
-                dates.push(date);
-              }
-            }
-            dates
-              .sort((itemA, itemB) => itemA.getTime() - itemB.getTime())
-              .map(date => {
-                return date
-                  .toISOString()
-                  .slice(0, 10)
-                  .split('-')
-                  .reverse()
-                  .join('-');
-              })
-              .forEach(date => {
-                if (!uniqueDates.includes(date)) {
-                  uniqueDates.push(date);
-                }
-              });
-          }),
-          map(result => {
-
-          })
-        );
   }
 }
