22f5af377f4bb3c57b6ee0581d8ea10b128d755a
Yarovji <39587257+Yarovji@users.noreply.github.com>
2019-04-11 18:35:27 +0300

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ShellGuard } from './services/shell.guard';
-import { AdminPanelGuard } from './services/admin-panel.guard';
-import { TeachersGuard } from './services/teachers.guard';
-import { StudentGuard } from './services/student.guard';
 
 const routes: Routes = [
   {
@@ -11,22 +8,10 @@ const routes: Routes = [
     loadChildren: './login/login.module#LoginModule'
   },
   {
-    path: 'admin-panel',
-    canActivate: [AdminPanelGuard],
-    canLoad: [AdminPanelGuard],
-    loadChildren: './admin-panel/admin-panel.module#AdminPanelModule'
-  },
-  {
-    path: 'journal',
-    canActivate: [TeachersGuard],
-    canLoad: [TeachersGuard],
-    loadChildren: './journal/journal.module#JournalModule'
-  },
-  {
-    path: 'student-book',
-    canActivate: [StudentGuard],
-    canLoad: [StudentGuard],
-    loadChildren: './student-book/student-book.module#StudentBookModule'
+    path: '',
+    loadChildren: './shell/shell.module#ShellModule',
+    canActivate: [ShellGuard],
+    canLoad: [ShellGuard]
   },
   {
     path: '',
