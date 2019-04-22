2761d89e33904b7c7bc7a113ba035a732bdb84ae
trChris <christinagr123123@gmail.com>
2019-04-22 23:44:01 +0300

    Change name of the section depending on active router in footer

--- src/app/admin-panel/admin-panel/footer-menu/footer-menu.component.ts
+++ src/app/admin-panel/admin-panel/footer-menu/footer-menu.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-admin-footer-menu',
@@ -46,7 +46,39 @@ export class AdminFooterMenuComponent implements OnInit {
     }
   ];
 
-  constructor(public auth: AuthService) { }
+  public iconActive: string;
+  public nameActive: string;
+  public pathActive: string;
+
+  constructor(private router: Router) { }
+
+  activeRoute(): boolean {
+    [this.pathActive, this.iconActive, this.nameActive] =
+      [this.routesMore[0].path, this.routesMore[0].icon, this.routesMore[0].name];
+    switch (this.router.url) {
+      case '/admin-panel/subjects':
+        [this.pathActive, this.iconActive, this.nameActive] =
+          [this.routesMore[0].path, this.routesMore[0].icon, this.routesMore[0].name];
+        break;
+      case '/journals':
+        [this.pathActive, this.iconActive, this.nameActive] =
+          [this.routesMore[1].path, this.routesMore[1].icon, this.routesMore[1].name];
+        break;
+      case '/admin-panel/schedule':
+        [this.pathActive, this.iconActive, this.nameActive] =
+          [this.routesMore[2].path, this.routesMore[2].icon, this.routesMore[2].name];
+        break;
+      case '/admin-panel/teacher-connection':
+        [this.pathActive, this.iconActive, this.nameActive] =
+          [this.routesMore[3].path, this.routesMore[3].icon, this.routesMore[3].name];
+        break;
+      case '/admin-panel/new-year-transition':
+        [this.pathActive, this.iconActive, this.nameActive] =
+          [this.routesMore[4].path, this.routesMore[4].icon, this.routesMore[4].name];
+        break;
+    }
+    return true;
+  }
 
   ngOnInit() {
   }
