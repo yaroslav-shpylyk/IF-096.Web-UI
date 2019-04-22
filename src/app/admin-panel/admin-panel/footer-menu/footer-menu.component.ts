0d826d97e5430a0ffc2637eefc4b9f8e6ba51203
trChris <christinagr123123@gmail.com>
2019-04-22 13:11:10 +0300

    Show sections with ngFor Directive

--- src/app/admin-panel/admin-panel/footer-menu/footer-menu.component.ts
+++ src/app/admin-panel/admin-panel/footer-menu/footer-menu.component.ts
@@ -8,6 +8,44 @@ import { AuthService } from '../../../services/auth.service';
 })
 export class AdminFooterMenuComponent implements OnInit {
 
+  public routes = [
+    { path: '/admin-panel/students',
+      icon: 'school',
+      name: 'УЧНІ'
+    },
+    { path: '/admin-panel/teachers',
+      icon: 'person',
+      name: 'ВЧИТЕЛІ'
+    },
+    { path: '/admin-panel/groups',
+      icon: 'group_work',
+      name: 'КЛАСИ'
+    }
+  ];
+
+  public routesMore = [
+    { path: '/admin-panel/subjects',
+      icon: 'library_books',
+      name: 'Предмети'
+    },
+    { path: '/journals',
+      icon: 'collections_bookmark',
+      name: 'Журнали'
+    },
+    { path: '/admin-panel/schedule',
+      icon: 'today',
+      name: 'Розклад'
+    },
+    { path: '/admin-panel/teacher-connection',
+      icon: 'person_add',
+      name: 'Додати вчителя до журналу'
+    },
+    { path: '/admin-panel/new-year-transition',
+      icon: 'queue',
+      name: 'Новий навчальний рік'
+    }
+  ];
+
   constructor(public auth: AuthService) { }
 
   ngOnInit() {
--- src/app/admin-panel/admin-panel/header-menu/header-menu.component.ts
+++ src/app/admin-panel/admin-panel/header-menu/header-menu.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../../../services/auth.service';
+import {Component, OnInit} from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
 
 @Component({
   selector: 'app-admin-header-menu',
@@ -7,8 +7,46 @@ import { AuthService } from '../../../services/auth.service';
   styleUrls: ['./header-menu.component.scss']
 })
 export class AdminHeaderMenuComponent implements OnInit {
+  public routes = [
+    { path: '/admin-panel/students',
+      icon: 'school',
+      name: 'УЧНІ'
+    },
+    { path: '/admin-panel/teachers',
+      icon: 'person',
+      name: 'ВЧИТЕЛІ'
+    },
+    { path: '/admin-panel/groups',
+      icon: 'group_work',
+      name: 'КЛАСИ'
+    }
+  ];
 
-  constructor(public auth: AuthService) { }
+  public routesMore = [
+    { path: '/admin-panel/subjects',
+      icon: 'library_books',
+      name: 'Предмети'
+    },
+    { path: '/journals',
+      icon: 'collections_bookmark',
+      name: 'Журнали'
+    },
+    { path: '/admin-panel/schedule',
+      icon: 'today',
+      name: 'Розклад'
+    },
+    { path: '/admin-panel/teacher-connection',
+      icon: 'person_add',
+      name: 'Додати вчителя до журналу'
+    },
+    { path: '/admin-panel/new-year-transition',
+      icon: 'queue',
+      name: 'Новий навчальний рік'
+    }
+  ];
+
+  constructor(public auth: AuthService) {
+  }
 
   ngOnInit() {
   }
