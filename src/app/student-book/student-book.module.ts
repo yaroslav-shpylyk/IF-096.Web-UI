ac885092217b15bc32b38109d2a8aaca54df198c
Mykola Trots <trots86@gmail.com>
2019-04-23 20:07:42 +0300

    created html tempate, seted data from server by deys

--- src/app/student-book/student-book.module.ts
+++ src/app/student-book/student-book.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 
 import { StudentBookRoutingModule } from './student-book-routing.module';
 import { StudentBookComponent } from './student-book/student-book.component';
+import { MaterialModule } from '../material.module';
 
 @NgModule({
   declarations: [StudentBookComponent],
   imports: [
     CommonModule,
-    StudentBookRoutingModule
+    StudentBookRoutingModule,
+    MaterialModule
   ]
 })
 export class StudentBookModule { }
--- src/app/student-book/student-book/student-book.component.ts
+++ src/app/student-book/student-book/student-book.component.ts
@@ -1,15 +1,125 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {StudentBookService} from '../../services/student-book.service';
+import {StudentBookData} from 'src/app/models/student-book-data';
+import {MatTableDataSource} from '@angular/material';
 
 @Component({
   selector: 'app-student-book',
   templateUrl: './student-book.component.html',
-  styleUrls: ['./student-book.component.scss']
+  styleUrls: ['./student-book.component.scss'],
 })
 export class StudentBookComponent implements OnInit {
 
-  constructor() { }
+  public studentData: StudentBookData[];
+  public displayedColumns: string[] = ['position', 'lesson', 'homeWork', 'mark', 'note'];
+
+  constructor(private studentBookService: StudentBookService) {
+  }
+
+  value = '';
+  serverData;
+  groupArr;
+
+  /*groupArr = this.serverData.reduce((r, {date}) => {
+    if (!r.some(o => o.date === date)) {
+      r.push({date, dateItem: this.serverData.filter(v => v.date === date)});
+    }
+    return r;
+  }, []);*/
+
+  /*  array = [
+      {
+        group: [2018, 10, 15],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3068,
+        lessonNumber: 1,
+        mark: 0,
+        note: '',
+        subjectName: 'Математика'
+      },
+      {
+        group: [2018, 10, 15],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3069,
+        lessonNumber: 2,
+        mark: 0,
+        note: '',
+        subjectName: 'Біологія'
+      },
+      {
+        group: [2018, 10, 15],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3070,
+        lessonNumber: 3,
+        mark: 0,
+        note: '',
+        subjectName: 'Українська мова'
+      },
+      {
+        group: [2018, 10, 16],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3070,
+        lessonNumber: 1,
+        mark: 0,
+        note: '',
+        subjectName: 'Українська мова'
+      },
+      {
+        group: [2018, 10, 16],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3070,
+        lessonNumber: 2,
+        mark: 0,
+        note: '',
+        subjectName: 'Українська мова'
+      },
+      {
+        group: [2018, 10, 17],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3070,
+        lessonNumber: 1,
+        mark: 0,
+        note: '',
+        subjectName: 'Українська мова'
+      },
+      {
+        group: [2018, 10, 17],
+        homeWork: 'Домашнє завдання #70, #76',
+        homeworkFileId: null,
+        lessonId: 3070,
+        lessonNumber: 2,
+        mark: 0,
+        note: '',
+        subjectName: 'Українська мова'
+      }
+    ];*/
 
   ngOnInit() {
   }
 
+  /**
+   * Method gets data from input and send it to service
+   * @param value from input
+   */
+  onEnter(value: string) {
+    this.value = value;
+    console.log(value);
+    this.studentBookService.getInputDate(this.value);
+    this.studentBookService.getStudentBook().subscribe(result => {
+      this.serverData = result;
+      console.log(this.serverData);
+      this.groupArr = this.serverData.reduce((r, {date}) => {
+        if (!r.some(o => o.date.toString() === date.toString())) {
+          r.push({date, groupItem: this.serverData.filter(v => v.date.toString() === date.toString())});
+        }
+        return r;
+      }, []);
+    });
+  }
 }
