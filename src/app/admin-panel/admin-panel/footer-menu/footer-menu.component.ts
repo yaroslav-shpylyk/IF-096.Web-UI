047bed4f4d29357baa49d45c9e79ddbd713ae067
trChris <christinagr123123@gmail.com>
2019-04-24 18:08:04 +0300

    Made annotations

--- src/app/admin-panel/admin-panel/footer-menu/footer-menu.component.ts
+++ src/app/admin-panel/admin-panel/footer-menu/footer-menu.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {Router} from '@angular/router';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-admin-footer-menu',
@@ -8,78 +9,97 @@ import {Router} from '@angular/router';
 })
 export class AdminFooterMenuComponent implements OnInit {
 
-  public routes = [
-    { path: '/admin-panel/students',
+  public routes = [ // array for static button links on bottom navigation
+    {
+      path: '/admin-panel/students',
       icon: 'school',
       name: 'Учні'
     },
-    { path: '/admin-panel/teachers',
+    {
+      path: '/admin-panel/teachers',
       icon: 'person',
       name: 'Вчителі'
     }
   ];
 
-  public routesMore = [
-    { path: '/admin-panel/groups',
+  public routesMore = [ // array for button links on 3 dots menu
+    {
+      path: '/admin-panel/groups',
       icon: 'group_work',
       name: 'Класи'
     },
-    { path: '/admin-panel/subjects',
+    {
+      path: '/admin-panel/subjects',
       icon: 'library_books',
       name: 'Предмети'
     },
-    { path: '/journals',
+    {
+      path: '/journals',
       icon: 'collections_bookmark',
       name: 'Журнали'
     },
-    { path: '/admin-panel/schedule',
+    {
+      path: '/admin-panel/schedule',
       icon: 'today',
       name: 'Розклад'
     },
-    { path: '/admin-panel/teacher-connection',
+    {
+      path: '/admin-panel/teacher-connection',
       icon: 'person_add',
       name: 'Додати вчителя до журналу'
     },
-    { path: '/admin-panel/new-year-transition',
+    {
+      path: '/admin-panel/new-year-transition',
       icon: 'queue',
       name: 'Новий навчальний рік'
     }
   ];
 
-  public iconActive: string;
-  public nameActive: string;
-  public pathActive: string;
-  public active;
+  public iconActive: string; // icon for third section in bottom navigation menu
+  public nameActive: string; // name
+  public pathActive: string; // path
+  public active: boolean; // active route
 
-  constructor(private router: Router) {
-    router.events.subscribe(() => {
-      this.activeRoute();
-    });
+  constructor(public auth: AuthService, private router: Router) {
   }
 
+  /**
+   * change third section in bottom navigation
+   * depending on selected routing from 3 dots menu
+   * show name, icon, route depending on selected section
+   * Classes is shown by default
+   */
   activeRoute() {
-    for (const i in this.routesMore) {
-      if (this.router.url ===  this.routesMore[i].path) {
-          [this.pathActive, this.iconActive, this.nameActive] =
-            [this.routesMore[i].path, this.routesMore[i].icon, this.truncateName(this.routesMore[i].name)];
-          this.active = true;
+    [this.pathActive, this.iconActive, this.nameActive] =
+      [this.routesMore[0].path, this.routesMore[0].icon, this.routesMore[0].name]; // show Classes by default
+    for (const i in this.routesMore) { // link buttons from 3 dots menu
+      if (this.router.url === this.routesMore[i].path) { // show path, icon, route depending on route path
+        [this.pathActive, this.iconActive, this.nameActive] =
+          [this.routesMore[i].path, this.routesMore[i].icon, this.truncateName(this.routesMore[i].name, 10)];
+        this.active = true; // highlight section
       }
     }
-    for (const i in this.routes) {
-      if (this.router.url ===  this.routes[i].path) {
-        this.active = false;
+    for (const i in this.routes) { // link buttons from static menu
+      if (this.router.url === this.routes[i].path || this.router.url === '/admin-panel') {
+        this.active = false; // remove class for highlighting section in third section
       }
     }
   }
 
-  truncateName(name): string {
-    return name.length > 10 ? name.slice(0, 10) + '…' : name;
+  /**
+   * truncate name if it is longer than specified characters in bottom navigation menu
+   * @param name of route path
+   * @param size of allowed characters
+   * @returns name or truncated name (if it is too long)
+   */
+  truncateName(name, size): string {
+    return name.length > size ? name.slice(0, size) + '…' : name;
   }
 
   ngOnInit() {
-    [this.pathActive, this.iconActive, this.nameActive] =
-      [this.routesMore[0].path, this.routesMore[0].icon, this.routesMore[0].name];
-    this.activeRoute();
+    this.router.events.subscribe(() => {
+      this.activeRoute();
+    }); // call on every routing change
+    this.activeRoute(); // call to check which section is selected when component is created
   }
-
 }
--- src/app/admin-panel/admin-panel/header-menu/header-menu.component.ts
+++ src/app/admin-panel/admin-panel/header-menu/header-menu.component.ts
@@ -1,5 +1,6 @@
-import {Component, HostListener, OnInit} from '@angular/core';
-import {Router} from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../../services/auth.service';
 
 @Component({
   selector: 'app-admin-header-menu',
@@ -7,96 +8,98 @@ import {Router} from '@angular/router';
   styleUrls: ['./header-menu.component.scss']
 })
 export class AdminHeaderMenuComponent implements OnInit {
-  public routes = [
-    { path: '/admin-panel/students',
+
+  public routes = [ // array for static button links in header
+    {
+      path: '/admin-panel/students',
       icon: 'school',
       name: 'Учні'
     },
-    { path: '/admin-panel/teachers',
+    {
+      path: '/admin-panel/teachers',
       icon: 'person',
       name: 'Вчителі'
     }
   ];
 
-  public routesMore = [
-    { path: '/admin-panel/groups',
+  public routesMore = [ // array for button links on More menu
+    {
+      path: '/admin-panel/groups',
       icon: 'group_work',
       name: 'Класи'
     },
-    { path: '/admin-panel/subjects',
+    {
+      path: '/admin-panel/subjects',
       icon: 'library_books',
       name: 'Предмети'
     },
-    { path: '/journals',
+    {
+      path: '/journals',
       icon: 'collections_bookmark',
       name: 'Журнали'
     },
-    { path: '/admin-panel/schedule',
+    {
+      path: '/admin-panel/schedule',
       icon: 'today',
       name: 'Розклад'
     },
-    { path: '/admin-panel/teacher-connection',
+    {
+      path: '/admin-panel/teacher-connection',
       icon: 'person_add',
       name: 'Додати вчителя до журналу'
     },
-    { path: '/admin-panel/new-year-transition',
+    {
+      path: '/admin-panel/new-year-transition',
       icon: 'queue',
       name: 'Новий навчальний рік'
     }
   ];
 
-  public iconActive: string;
-  public nameActive: string;
-  public pathActive: string;
-  public active;
-  private commonDisplay;
-  private retinaDisplay;
-  public admin = 'Адміністратор';
+  public iconActive: string; // icon for fourth section in header menu (after More menu)
+  public nameActive: string; // name
+  public pathActive: string; // path
+  public active: boolean; // active route
 
-  constructor(private router: Router) {
-    router.events.subscribe(() => {
-      this.activeRoute();
-    });
+  constructor(public auth: AuthService, private router: Router) {
   }
-  activeRoute(): boolean {
-    for (const i in this.routesMore) {
-      if (this.router.url ===  this.routesMore[i].path) {
+
+  /**
+   * change fourth section in header menu
+   * depending on selected routing from More menu
+   * show name, icon, route depending on selected section
+   * Classes is shown by default
+   */
+  activeRoute() {
+    [this.pathActive, this.iconActive, this.nameActive] =
+      [this.routesMore[0].path, this.routesMore[0].icon, this.routesMore[0].name]; // show Classes by default
+    for (const i in this.routesMore) { // link buttons from More menu
+      if (this.router.url === this.routesMore[i].path) { // show path, icon, route depending on route path
         [this.pathActive, this.iconActive, this.nameActive] =
-          [this.routesMore[i].path, this.routesMore[i].icon, this.truncateName(this.routesMore[i].name, 10)];
-        this.active = true;
+          [this.routesMore[i].path, this.routesMore[i].icon, this.truncateName(this.routesMore[i].name, 8)];
+        this.active = true; // highlight section
       }
     }
-    for (const i in this.routes) {
-      if (this.router.url ===  this.routes[i].path) {
-        this.active = false;
+    for (const i in this.routes) { // link buttons from static menu
+      if (this.router.url === this.routes[i].path || this.router.url === '/admin-panel') {
+        this.active = false; // remove class for highlighting section in fourth section
       }
     }
-    return true;
   }
 
+  /**
+   * truncate name if it is longer than specified characters in header menu
+   * @param name of route path
+   * @param size of allowed characters
+   * @returns name or truncated name (if it is too long)
+   */
   truncateName(name, size): string {
     return name.length > size ? name.slice(0, size) + '…' : name;
   }
 
-  /**
-   * listen to window width resizing
-   * to get current device screen width
-   */
-  @HostListener('window:resize', ['$event'])
-  onResize() {
-    this.commonDisplay = window.matchMedia('(max-width: 700px) and (min-width: 600px)').matches; // most smartphones in portrait mode
-    this.retinaDisplay = window.matchMedia('(max-width: 700px) ' +
-      'and (min-resolution: 2dppx) and (orientation: portrait)').matches; // smartphones with retina display in portrait mode
-    if (this.commonDisplay || this.retinaDisplay) {
-    this.truncateName(this.admin, 6);
-  }
-
-  }
-
   ngOnInit() {
-    [this.pathActive, this.iconActive, this.nameActive] =
-      [this.routesMore[0].path, this.routesMore[0].icon, this.routesMore[0].name];
-    this.activeRoute();
+    this.router.events.subscribe(() => {
+      this.activeRoute(); // call on every routing change
+    });
+    this.activeRoute(); // call to check which section is selected when component is created
   }
-
 }
