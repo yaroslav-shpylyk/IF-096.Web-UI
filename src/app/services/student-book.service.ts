0cf7b81466d533238bb940068da318d3d91128ec
Mykola Trots <trots86@gmail.com>
2019-04-25 12:13:51 +0300

    fixed format of dates heading in the tables

---
+++ src/app/services/student-book.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { StudentBookData } from '../models/student-book-data';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StudentBookService {
+  inputDate: string;
+
+  constructor(private http: HttpClient) { }
+
+  /**
+   * Method gets data from server, using weekStartDate as day of starting
+   * @returns schedule for getInputDate week
+   */
+  public getStudentBook(): Observable<StudentBookData[]> {
+    return this.http.get('/diaries', {params: {weekStartDate : this.inputDate} } )
+      .pipe(
+        map((result: {status: any, data: StudentBookData[]}) => result.data)
+      );
+  }
+
+  /**
+   * Method which takes Input Date from component
+   * @param value from method onEnter in student-book.component.ts
+   */
+  public getInputDate(value) {
+    this.inputDate = value;
+  }
+}
--- src/app/student-book/student-book/student-book.component.ts
+++ src/app/student-book/student-book/student-book.component.ts
@@ -1,157 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { StudentBookService } from '../../services/student-book.service';
 import { StudentBookData } from 'src/app/models/student-book-data';
-import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
-import { MatDatepickerInputEvent } from '@angular/material';
+import { DateAdapter } from '@angular/material/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-student-book',
   templateUrl: './student-book.component.html',
-  styleUrls: ['./student-book.component.scss'],
-  providers: [
-    // The locale would typically be provided on the root module of your application. We do it at
-    // the component level here, due to limitations of our example generation script.
-    {provide: MAT_DATE_LOCALE, useValue: 'uk-UA'},
-
-    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
-    // `MatMomentDateModule` in your applications root module. We provide it at the component level
-    // here, due to limitations of our example generation script.
-    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
-  ]
+  styleUrls: ['./student-book.component.scss']
 })
+
 export class StudentBookComponent implements OnInit {
+
   public studentData: StudentBookData[];
   public displayedColumns: string[] = ['position', 'lesson', 'homeWork', 'mark', 'note'];
-
-  constructor(private studentBookService: StudentBookService, private adapter: DateAdapter<any>) {
-  }
-
   public value;
   public serverData;
   public groupArr;
   public pickerDate;
+  public date = new FormControl(new Date());
 
-  /*groupArr = this.serverData.reduce((r, {date}) => {
-    if (!r.some(o => o.date === date)) {
-      r.push({date, dateItem: this.serverData.filter(v => v.date === date)});
-    }
-    return r;
-  }, []);*/
-
-  /*  array = [
-      {
-        group: [2018, 10, 15],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3068,
-        lessonNumber: 1,
-        mark: 0,
-        note: '',
-        subjectName: 'Математика'
-      },
-      {
-        group: [2018, 10, 15],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3069,
-        lessonNumber: 2,
-        mark: 0,
-        note: '',
-        subjectName: 'Біологія'
-      },
-      {
-        group: [2018, 10, 15],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3070,
-        lessonNumber: 3,
-        mark: 0,
-        note: '',
-        subjectName: 'Українська мова'
-      },
-      {
-        group: [2018, 10, 16],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3070,
-        lessonNumber: 1,
-        mark: 0,
-        note: '',
-        subjectName: 'Українська мова'
-      },
-      {
-        group: [2018, 10, 16],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3070,
-        lessonNumber: 2,
-        mark: 0,
-        note: '',
-        subjectName: 'Українська мова'
-      },
-      {
-        group: [2018, 10, 17],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3070,
-        lessonNumber: 1,
-        mark: 0,
-        note: '',
-        subjectName: 'Українська мова'
-      },
-      {
-        group: [2018, 10, 17],
-        homeWork: 'Домашнє завдання #70, #76',
-        homeworkFileId: null,
-        lessonId: 3070,
-        lessonNumber: 2,
-        mark: 0,
-        note: '',
-        subjectName: 'Українська мова'
-      }
-    ];*/
-
+  constructor(private studentBookService: StudentBookService, private adapter: DateAdapter<any>) {
+  }
   ngOnInit() {
   }
-
   /**
-   * Method gets data from input and send it to service
-   * @param value from input
+   * Method gets data from datePicker and send it to service
+   * @param event from input
    */
-/*  onEnter(value: string) {
-    this.value = value;
-    console.log(value);
-    this.studentBookService.getInputDate(this.value);
-    this.studentBookService.getStudentBook().subscribe(result => {
-      this.serverData = result;
-      console.log(this.serverData);
-      this.groupArr = this.serverData.reduce((r, {date}) => {
-        if (!r.some(o => o.date.toString() === date.toString())) {
-          r.push({date, groupItem: this.serverData.filter(v => v.date.toString() === date.toString())});
-        }
-        return r;
-      }, []);
-    });
-  }*/
 
-
-  /*addEvent(event: MatDatepickerInputEvent<Date>) {
-    console.log(event.value.toISOString().slice(0, 10));
-    this.pickerDate = event.value.toISOString().slice(0, 10);
-    this.studentBookService.getInputDate(this.pickerDate);
-    this.studentBookService.getStudentBook().subscribe(result => {
-      this.serverData = result;
-      console.log(this.serverData);
-      this.groupArr = this.serverData.reduce((r, {date}) => {
-        if (!r.some(o => o.date.toString() === date.toString())) {
-          r.push({date, groupItem: this.serverData.filter(v => v.date.toString() === date.toString())});
-        }
-        return r;
-      }, []);
-    });
-  }*/
 
   addEvent(event: any) {
     console.log(event.value.format('YYYY-MM-DD'));
@@ -160,11 +37,11 @@ export class StudentBookComponent implements OnInit {
     this.studentBookService.getStudentBook().subscribe(result => {
       this.serverData = result;
       console.log(this.serverData);
-      this.groupArr = this.serverData.reduce((r, {date}) => {
-        if (!r.some(o => o.date.toString() === date.toString())) {
-          r.push({date, groupItem: this.serverData.filter(v => v.date.toString() === date.toString())});
+      this.groupArr = this.serverData.reduce((reducer, {date}) => {
+        if (!reducer.some(o => o.date.toString() === date.toString())) {
+          reducer.push({date, groupItem: this.serverData.filter(v => v.date.toString() === date.toString())});
         }
-        return r;
+        return reducer;
       }, []);
     });
   }
