0a0a6909812b89c395ea2fcb5ba5862fd61d84fc
trChris <christinagr123123@gmail.com>
2019-04-28 00:14:34 +0300

    Added avatar and name by userId into header

--- src/app/journal/header-menu/header-menu.component.ts
+++ src/app/journal/header-menu/header-menu.component.ts
@@ -1,17 +1,42 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import {HeaderService} from '../../services/header.service';
 
 @Component({
   selector: 'app-teacher-header-menu',
   templateUrl: './header-menu.component.html',
   styleUrls: ['./header-menu.component.scss']
 })
-export class TeacherHeaderMenuComponent implements OnInit {
-  public fullName;
 
-  constructor(public auth: AuthService) { }
+
+export class TeacherHeaderMenuComponent implements OnInit {
+  public teacherData;
+  public fullName;
+  public avatar;
+  public showAbbr: boolean;
+  public showAvatar: boolean;
+
+  constructor(public auth: AuthService,
+              public headerService: HeaderService) { }
+
+  showFullName() {
+    this.headerService.getTeacher(this.auth.getUserId()).subscribe(result => {
+      this.teacherData = result;
+      this.fullName = `${result.firstname} ${result.lastname}`;
+      if(this.teacherData.avatar === '/img/profile.png'){
+        this.showAbbr = true;
+        this.showAvatar = false;
+      }else if (this.teacherData.avatar) {
+        this.showAvatar = true;
+        this.showAbbr = false;
+        this.avatar = this.teacherData.avatar;
+
+      }
+    });
+  }
 
   ngOnInit() {
+     this.showFullName();
 
   }
 
--- src/app/student-book/header-menu/header-menu.component.ts
+++ src/app/student-book/header-menu/header-menu.component.ts
@@ -1,9 +1,7 @@
-import {Component, Inject, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import {Student} from '../../models/student';
-import {StudentsService} from '../../services/students.service';
-import {ActivatedRoute} from '@angular/router';
-import {AvatarComponent} from '../../shared/avatar/avatar.component';
+import { HeaderService } from '../../services/header.service';
+import { Student} from '../../models/student';
 
 @Component({
   selector: 'app-student-header-menu',
@@ -11,12 +9,34 @@ import {AvatarComponent} from '../../shared/avatar/avatar.component';
   styleUrls: ['./header-menu.component.scss']
 })
 export class StudentHeaderMenuComponent implements OnInit {
+  public studentData;
   public fullName;
+  public avatar;
+  public showAbbr: boolean;
+  public showAvatar: boolean;
 
-  constructor(public auth: AuthService) { }
+  constructor(public auth: AuthService,
+              public headerService: HeaderService) { }
+
+  showFullName() {
+    this.headerService.getStudent(this.auth.getUserId()).subscribe((student: Student) => {
+      this.studentData = student;
+      this.fullName = `${this.studentData.firstname} ${this.studentData.lastname}`;
+
+      if(this.studentData.avatar === '/img/profile.png'){
+        this.showAbbr = true;
+        this.showAvatar = false;
+      }else if (this.studentData.avatar) {
+        this.showAvatar = true;
+        this.showAbbr = false;
+        this.avatar = this.studentData.avatar;
+
+      }
+    });
+  }
 
   ngOnInit() {
-
+    this.showFullName();
   }
 
 }
