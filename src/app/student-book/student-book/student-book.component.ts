2649543183fafef897337150ea54032f1bf1010c
Mykola Trots <trots86@gmail.com>
2019-04-29 23:25:14 +0300

    added two kind of views of schedule

--- src/app/student-book/student-book/student-book.component.ts
+++ src/app/student-book/student-book/student-book.component.ts
@@ -1,13 +1,7 @@
-import {Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { StudentBookService } from '../../services/student-book.service';
-import { StudentBookData } from 'src/app/models/student-book-data';
-import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
-
-// Depending on whether rollup is used, moment needs to be imported differently.
-// Since Moment.js doesn't have a default export, we normally need to import using the `* as`
-// syntax. However, rollup creates a synthetic default module and we thus need to import it using
-// the `default as` syntax.
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
 import * as _moment from 'moment';
 
 const moment = _moment;
@@ -17,17 +11,13 @@ const moment = _moment;
   templateUrl: './student-book.component.html',
   styleUrls: ['./student-book.component.scss'],
   providers: [
-    // `MomentDateAdapter` and `MAT_MOMENT_DATE_FORMATS` can be automatically provided by importing
-    // `MatMomentDateModule` in your applications root module. We provide it at the component level
-    // here, due to limitations of our example generation script.
     {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
     {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
-  ],
+  ]
 })
 
 export class StudentBookComponent implements OnInit {
 
-  public studentData: StudentBookData[];
   public displayedColumns: string[] = ['position', 'lesson', 'homeWork', 'mark', 'note'];
   public value;
   public serverData;
@@ -37,57 +27,45 @@ export class StudentBookComponent implements OnInit {
   public showTable: boolean;
   public dateValue = moment();
 
-  constructor(private studentBookService: StudentBookService, private adapter: DateAdapter<any>) {
+  constructor(private studentBookService: StudentBookService) {
   }
+
   ngOnInit() {
     this.showSchedule();
   }
 
+  /**
+   * Method which works with button BackDate, substract 7 days from current date.
+   */
   backDate() {
     this.dateValue = moment(this.dateValue.subtract(7, 'd').format('YYYY-MM-DD'));
     console.log(this.dateValue);
     this.showSchedule();
   }
+
+  /**
+   * Method which works with button ForwardDate, add 7 days from current date.
+   */
   forwardDate() {
     this.dateValue = moment(this.dateValue.add(7, 'd').format('YYYY-MM-DD'));
     console.log(this.dateValue);
     this.showSchedule();
   }
 
+  /**
+   * Method which load schedule on the page
+   */
   showSchedule() {
-    this.pickerDate = moment(this.dateValue.format('YYYY-MM-DD'));
+    this.pickerDate = moment(this.dateValue.format('YYYY-MM-DD')); // dateValue from datePicker
     console.log('pickerDate in showSchedule:' + this.pickerDate.format('YYYY-MM-DD'));
-    this.studentBookService.getInputDate(this.pickerDate.format('YYYY-MM-DD'));
+    this.studentBookService.getInputDate(this.pickerDate.format('YYYY-MM-DD')); // send date from datePicker to server
     this.studentBookService.getStudentBook().subscribe(result => {
-      this.serverData = result;
+      this.serverData = result; // data from server
       console.log(this.serverData);
       if (this.serverData.length) {
         this.showMessage = false;
         this.showTable = true;
-        this.groupArr = this.serverData.reduce((reducer, {date}) => {
-          if (!reducer.some(i => i.date.toString() === date.toString())) {
-            reducer.push({date, groupItem: this.serverData.filter(value => value.date.toString() === date.toString())});
-          }
-          return reducer;
-        }, []);
-      } else {
-        this.showMessage = true;
-        this.showTable = false;
-      }
-    });
-  }
-
-  getDatePicker(event: any) {
-    this.dateValue = moment(event.value.format('YYYY-MM-DD'));
-    this.pickerDate = this.dateValue;
-    console.log(this.pickerDate);
-    this.studentBookService.getInputDate(this.pickerDate.format('YYYY-MM-DD'));
-    this.studentBookService.getStudentBook().subscribe(result => {
-      this.serverData = result;
-      console.log(this.serverData);
-      if (this.serverData.length) {
-        this.showMessage = false;
-        this.showTable = true;
+        // sort data from server by date
         this.groupArr = this.serverData.reduce((reducer, {date}) => {
           if (!reducer.some(i => i.date.toString() === date.toString())) {
             reducer.push({date, groupItem: this.serverData.filter(value => value.date.toString() === date.toString())});
