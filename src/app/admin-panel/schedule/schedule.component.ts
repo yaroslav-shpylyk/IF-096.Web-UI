d5703b6c171952d1e81f525498604e0c454ace8b
Tetiana Mysiuk <tivasyuk@i.ua>
2019-04-30 20:14:19 +0300

    add validators for date

--- src/app/admin-panel/schedule/schedule.component.ts
+++ src/app/admin-panel/schedule/schedule.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
-import { FormGroup, FormBuilder, Validators, FormArray } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormArray, FormControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 import { ScheduleData } from '../../models/schedule-data';
 import { ClassData } from '../../models/class-data';
 import { SubjectData } from '../../models/subject-data';
@@ -8,10 +8,19 @@ import { SubjectService } from '../../services/subject.service';
 import { ScheduleService } from '../../services/schedule.service';
 import { LessonData } from '../../models/lesson-data';
 
+import {MAT_MOMENT_DATE_FORMATS, MomentDateAdapter} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
+
 @Component({
   selector: 'app-schedule',
   templateUrl: './schedule.component.html',
-  styleUrls: ['./schedule.component.scss']
+  styleUrls: ['./schedule.component.scss'],
+
+  providers: [
+    {provide: MAT_DATE_LOCALE, useValue: 'ua-UA'},
+    {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ],
 })
 export class ScheduleComponent implements OnInit {
   scheduleData: ScheduleData;
@@ -44,7 +53,8 @@ export class ScheduleComponent implements OnInit {
     private frmBld: FormBuilder,
     private classService: ClassService,
     private subjectsService: SubjectService,
-    private scheduleService: ScheduleService
+    private scheduleService: ScheduleService,
+    private adapter: DateAdapter<any>,
   ) { }
 
   ngOnInit() {
@@ -54,6 +64,7 @@ export class ScheduleComponent implements OnInit {
     this.subjectsService.getSubjects().subscribe(data => {
       this.arrSubjectsList = data;
     });
+    this.adapter.setLocale('ua');
     this.initForm();
   }
 
@@ -64,6 +75,36 @@ export class ScheduleComponent implements OnInit {
       dateTermEnd: [''],
       selectClass: ['', Validators.required]
     });
+
+    const dateValidators: ValidatorFn[] = [
+      Validators.required,
+      this.DateValidator
+    ];
+    this.frmSchedule.get('dateTermStart').setValidators(dateValidators);
+    this.frmSchedule.get('dateTermEnd').setValidators(dateValidators);
+  }
+
+  /**
+   * Method is a validator for the start and the end date of the period
+   * @param control - the control to which it is tied
+   * @returns - object of the type ValidationErrors if a validation error occurred,
+   * or null if validation was successful
+   */
+  private DateValidator(control: FormControl): ValidationErrors {
+    let dateStart: Date;
+    let errorMsg: string;
+    if (control === control.parent.get('dateTermStart')) {
+      dateStart = new Date();
+      errorMsg = 'Дата початку семестру не може бути в минулому';
+    } else {
+      dateStart = new Date(control.parent.get('dateTermStart').value);
+      errorMsg = 'Дата закінчення семестру не може раніше його початку';
+    }
+    const dateValue = new Date(control.value);
+    if (dateValue.getTime() < dateStart.getTime()) {
+      return {invalidDate: errorMsg};
+    }
+    return null;
   }
 
   /**
@@ -72,15 +113,19 @@ export class ScheduleComponent implements OnInit {
    * @returns - Schedule for selected class
    */
   selectedClass(classId: number) {
-
     this.scheduleService.getSchedule(classId).subscribe(data => {
       this.scheduleData = data;
-      //console.log(this.scheduleData);
+      // console.log(this.scheduleData);
     });
   }
 
-  onAddDailySubjects($event: FormArray, daySubjects: string): void {
-    this.emittedDays[daySubjects] = $event;
+  /**
+   * Method gets the schedule for each day from the child component
+   * @param event - Data for the daily schedule
+   * @param dailySubjects - Daily schedule name
+   */
+  onAddDailySubjects(event: FormArray, daySubjects: string): void {
+    this.emittedDays[daySubjects] = event;
   }
 
   /**
@@ -107,23 +152,23 @@ export class ScheduleComponent implements OnInit {
     }
     /* Handling form data */
 
-    this.scheduleData.startOfSemester = this.frmSchedule.controls['dateTermStart'].value;
-    this.scheduleData.endOfSemester = this.frmSchedule.controls['dateTermEnd'].value;
-    this.scheduleData.classId = this.frmSchedule.controls['selectClass'].value.id;
-    this.scheduleData.className = this.frmSchedule.controls['selectClass'].value;
+    this.scheduleData.startOfSemester = this.frmSchedule.controls.dateTermStart.value;
+    this.scheduleData.endOfSemester = this.frmSchedule.controls.dateTermEnd.value;
+    this.scheduleData.classId = this.frmSchedule.controls.selectClass.value.id;
+    this.scheduleData.className = this.frmSchedule.controls.selectClass.value;
 
     Object.keys(this.emittedDays).forEach(dailySubjects => {
       this.scheduleData[dailySubjects] = this.addDailyData(dailySubjects);
-    })
+    });
     console.log(this.scheduleData);
   }
 
   /**
    * The method generates data for the daily schedule
-   * @param dailySubjects - Daily schedule's name
+   * @param dailySubjects - Daily schedule name
    * @returns - Data for the daily schedule
    */
-  addDailyData(dailySubjects: string): LessonData[] {
+  private addDailyData(dailySubjects: string): LessonData[] {
     const dailyLesson: LessonData[] = [];
     for (let i = 0; i < (this.emittedDays[dailySubjects].value.length - 1); i++) {
       const lessonFirstGroup = {
@@ -131,10 +176,10 @@ export class ScheduleComponent implements OnInit {
         subjectId: 0,
         subjectName: '',
         subjectDescription: ''
-      }
+      };
       Object.keys(lessonFirstGroup).forEach(keyName =>
         lessonFirstGroup[keyName] = this.emittedDays[dailySubjects].value[i].firstGroup[keyName]);
-        lessonFirstGroup.lessonNumber = `${i + 1}`;
+      lessonFirstGroup.lessonNumber = `${i + 1}`;
       dailyLesson.push(lessonFirstGroup);
 
       if ('secondGroup' in this.emittedDays[dailySubjects].value[i]) {
@@ -143,11 +188,11 @@ export class ScheduleComponent implements OnInit {
           subjectId: 0,
           subjectName: '',
           subjectDescription: ''
-        }
+        };
         Object.keys(lessonSecondGroup).forEach(keyName =>
           lessonSecondGroup[keyName] = this.emittedDays[dailySubjects].value[i].secondGroup[keyName]);
-          lessonSecondGroup.lessonNumber = `${i + 1}`;
-          dailyLesson.push(lessonSecondGroup);
+        lessonSecondGroup.lessonNumber = `${i + 1}`;
+        dailyLesson.push(lessonSecondGroup);
       }
     }
     return dailyLesson;
