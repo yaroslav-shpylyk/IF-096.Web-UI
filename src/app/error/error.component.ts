db78b66dffaec2debed7b4c203801ca855933eb9
Yaroslav Virstiuk <yarovji@gmail.com>
2019-05-06 13:50:32 +0300

    resolve conversation

--- src/app/error/error.component.ts
+++ src/app/error/error.component.ts
@@ -3,8 +3,17 @@ import { MAT_DIALOG_DATA } from '@angular/material';
 
 @Component({
   selector: 'app-error',
-  templateUrl: './error.component.html',
-  styleUrls: ['./error.component.scss']
+  template:
+  `<div class="errorComponent">
+    <h1 mat-dialog-title>Виникла помилка!</h1>
+    <div mat-dialog-content>
+      <p class="mat-body-1">{{ data?.message }}</p>
+    </div>
+    <div mat-dialog-actions>
+      <button mat-button mat-dialog-close>Ок</button>
+    </div>
+ </div>`,
+  styles: [`.errorComponent {color: red;}`]
 })
 export class ErrorComponent {
   constructor(@Inject(MAT_DIALOG_DATA) public data: { message: string }) {}
