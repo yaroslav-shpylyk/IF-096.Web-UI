a2cf6f06b2ba6845d59ab9482ee488bca58edb18
Khrystyna Trots <christinagr123123@gmail.com>
2019-05-06 22:37:58 +0300

    Made isolated test in FooterComponent for checking user role for being admin, teacher or student

--- src/app/shared/footer/footer.component.spec.ts
+++ src/app/shared/footer/footer.component.spec.ts
@@ -1,25 +1,40 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthService } from '../../services/auth.service';
 import { FooterComponent } from './footer.component';
 
 describe('FooterComponent', () => {
-  let component: FooterComponent;
-  let fixture: ComponentFixture<FooterComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
-    })
-    .compileComponents();
-  }));
-
   beforeEach(() => {
-    fixture = TestBed.createComponent(FooterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [
+        AuthService
+      ]
+    });
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('check user role for being admin', () => {
+    let authService = TestBed.get(AuthService);
+    let footerComponent = new FooterComponent(authService);
+    spyOn(authService, 'getUserRole').and.returnValue('ROLE_ADMIN');
+    expect(footerComponent.isAdmin()).toBeTruthy();
+  });
+
+  it('check user role for being teacher', () => {
+    let authService = TestBed.get(AuthService);
+    let footerComponent = new FooterComponent(authService);
+    spyOn(authService, 'getUserRole').and.returnValue('ROLE_TEACHER');
+    expect(footerComponent.isTeacher()).toBeTruthy();
+  });
+
+  it('check user role for being student', () => {
+    let authService = TestBed.get(AuthService);
+    let footerComponent = new FooterComponent(authService);
+    spyOn(authService, 'getUserRole').and.returnValue('ROLE_USER');
+    expect(footerComponent.isStudent()).toBeTruthy();
   });
 });
