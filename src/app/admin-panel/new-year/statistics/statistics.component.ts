16f3f9031bc80e9d891c5aa7bd52d16790c7abdf
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-05-08 17:44:10 +0300

    added methods that receive statistics
    Description
    statistics component:
    	added methods getStatistic and agregateStatisticData
    	imported modules for MatTable, Chart and MatTabs
    	added initial settings for charts
    statistics template:
    	added MatTabs with 2 tabs for char and table
    new-year component, new-year service:
    	The ClassInfo interface was replaced by the ClassData interface to prevent duplicate code in the project

--- src/app/admin-panel/new-year/statistics/statistics.component.ts
+++ src/app/admin-panel/new-year/statistics/statistics.component.ts
@@ -1,4 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, ViewChild, ElementRef } from '@angular/core';
+import {MAT_DIALOG_DATA, MatTabChangeEvent} from '@angular/material';
+import { ChartDataSets } from 'chart.js';
+import { ClassData } from '../../../models/class-data';
+
+export interface YearTotalCount {
+  year: number;
+  total: number;
+  trend: number;
+}
+
+export interface TransitionData {
+  year?: {
+    enroll: number,
+    graduated: number
+    total: number,
+    trend: number
+  };
+}
 
 @Component({
   selector: 'app-statistics',
@@ -7,9 +25,131 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
-  constructor() { }
+  constructor( @Inject(MAT_DIALOG_DATA) public data: any ) { }
+
+  public transititionStatistic: any; //
+  public activeTabIndex = 0;
+  public displayedColumns: string[] = ['name', 'weight', 'trend']; //
+  public tableData: YearTotalCount[] = [];
+
+  public maxYNumber = 0;
+  public minYNumber = 0;
+  public lineChartData: ChartDataSets[] = [];
+  public chartLabels: Array<string> = [];
+  public lineChartType = 'bar';
+
+  @ViewChild('content') content: ElementRef;
+  @ViewChild('chart') chart: ElementRef;
+  @ViewChild('totalCountTable') totalCountTable: ElementRef;
+
+  public deltaChartOpt = {
+    data: [],
+    label: 'Дельта',
+    type: 'line',
+    fill: false,
+    lineTension: 0,
+    pointBackgroundColor: 'red',
+    pointHitRadius: 6
+  };
+
+  public enrollChartOpt = {
+    data: [],
+    label: 'Поступило',
+    backgroundColor: 'rgba(0, 255, 0, 0.8)'
+  };
+
+  public graduatedChartOpt = {
+    data: [],
+    label: 'Випуск',
+    backgroundColor: 'rgba(255, 0, 0, 0.8)'
+  };
+
+  public lineChartOptions: any = {
+    responsive: true,
+    scales : {
+      yAxes: [{
+        ticks: { max : this.maxYNumber, min : this.minYNumber }
+      }],
+      xAxes: [
+        {
+          stacked: true,
+          id: 'bar-x-axis1',
+          categoryPercentage: 0.5,
+        }
+      ],
+    },
+  };
+
+
 
   ngOnInit() {
+    this.transititionStatistic = this.getStatistic(this.data.allClasses);
+    console.log(this.transititionStatistic);
+    this.agregateStatisticData(this.transititionStatistic);
+  }
+
+  onTabClick(event: MatTabChangeEvent) {
+    this.activeTabIndex = event.index;
+  }
+
+  getStatistic(allClasses: ClassData[]): TransitionData {
+    const statistic: TransitionData = {};
+    const curDate = new Date();
+    const curYear = (curDate.getMonth() < 12 && curDate.getMonth() > 7) ? curDate.getFullYear() : curDate.getFullYear() - 1;
+    allClasses.forEach(
+      curClass => {
+        if ( curYear - curClass.classYear < 6 && curYear > curClass.classYear) {
+          const curClassYear = curClass.classYear;
+          if (!(curClassYear in statistic)) {
+            statistic[curClassYear] = { enroll: 0, graduated: 0, total: 0, trend: 0};
+          }
+          const classNameParts = curClass.className.split(/[-(]/);
+          const curClassNumber = classNameParts.length > 2 ? curClass.className.split(/[-(]/)[1] : curClass.className.split(/[-(]/)[0];
+          if ( +curClassNumber === 1 ) { statistic[curClassYear].enroll += curClass.numOfStudents; }
+          if ( +curClassNumber === 11 ) { statistic[curClassYear].graduated += curClass.numOfStudents; }
+          statistic[curClassYear].total += curClass.numOfStudents;
+          if ( (curClassYear - 1) in statistic ) {
+            statistic[curClassYear].trend = statistic[curClassYear].total - statistic[curClassYear - 1].total;
+          } else {
+            statistic[curClassYear].trend = statistic[curClassYear].total;
+          }
+          if ( (curClassYear + 1) in statistic ) {
+            statistic[curClassYear + 1].trend = statistic[curClassYear + 1].total - statistic[curClassYear].total;
+          }
+        }
+        }
+    );
+    return statistic;
+  }
+
+  agregateStatisticData(transititionStatistic: TransitionData): void {
+    const enrollData = [];
+    const graduatedData = [];
+    const deltaData = [];
+
+    for (const key in transititionStatistic) {
+      if (transititionStatistic.hasOwnProperty(key)) {
+        this.chartLabels.push(key);
+        this.maxYNumber = Math.max (this.maxYNumber, transititionStatistic[key].enroll, transititionStatistic[key].graduated);
+        this.minYNumber = Math.min (this.minYNumber, transititionStatistic[key].enroll - transititionStatistic[key].graduated);
+        enrollData.push(transititionStatistic[key].enroll);
+        graduatedData.push(transititionStatistic[key].graduated);
+        deltaData.push(transititionStatistic[key].enroll - transititionStatistic[key].graduated);
+        const curYearStats = transititionStatistic[key];
+        this.tableData.push({year: +key, total: curYearStats.total, trend: curYearStats.trend});
+      }
+    }
+
+    this.deltaChartOpt.data = deltaData;
+    this.enrollChartOpt.data = enrollData;
+    this.graduatedChartOpt.data = graduatedData;
+
+    this.lineChartOptions.scales.yAxes[0].ticks.max = this.maxYNumber + 1;
+    this.lineChartOptions.scales.yAxes[0].ticks.min = this.minYNumber - 1;
+
+    this.lineChartData.push(this.deltaChartOpt);
+    this.lineChartData.push(this.enrollChartOpt);
+    this.lineChartData.push(this.graduatedChartOpt);
   }
 
 }
