e7629c8f3c4e4248e66a4d01575908d5acca82a7
denys <den.hodovanets@gmail.com>
2019-05-11 23:59:50 +0300

    fix SW scope

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,7 +36,7 @@ import { environment } from '../environments/environment';
     FormsModule,
     ReactiveFormsModule,
     MatNativeDateModule,
-    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production, scope: '/' })
   ],
   providers: [
     AuthService,
