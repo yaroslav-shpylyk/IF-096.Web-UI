21e2f847298c7db00c157466d19cb6e4957f1feb
Yaroslav Virstiuk <yarovji@gmail.com>
2019-05-16 11:38:59 +0300

    write tests

--- src/app/services/groups.service.spec.ts
+++ src/app/services/groups.service.spec.ts
@@ -1,9 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GroupsService } from './groups.service';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
 
 describe('GroupsService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [GroupsService],
+    imports: [HttpClientTestingModule]
+  }));
 
   it('should be created', () => {
     const service: GroupsService = TestBed.get(GroupsService);
