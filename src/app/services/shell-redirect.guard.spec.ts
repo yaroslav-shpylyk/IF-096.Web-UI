37262f9525f343d14be40e6b46cade7b295de485
Khrystyna Trots <christinagr123123@gmail.com>
2019-05-20 13:52:15 +0300

    Implemented redirection for shell component and unit tests to it

--- src/app/services/shell-redirect.guard.spec.ts
+++ src/app/services/shell-redirect.guard.spec.ts
@@ -1,15 +1,30 @@
-import { TestBed, async, inject } from '@angular/core/testing';
-
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ShellRedirectGuard } from './shell-redirect.guard';
 
-describe('ShellRedirectGuard', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [ShellRedirectGuard]
-    });
-  });
+fdescribe('Guard for Shell component which should redirect', () => {
+  let shellRedirectGuard: ShellRedirectGuard;
+  const routerSnapshot: any = jasmine.createSpyObj<RouterStateSnapshot>('RouterStateSnapshot', ['toString']);
 
-  it('should ...', inject([ShellRedirectGuard], (guard: ShellRedirectGuard) => {
-    expect(guard).toBeTruthy();
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        ShellRedirectGuard,
+        { provide: RouterStateSnapshot, useValue: routerSnapshot }
+      ]
+    })
+      .compileComponents();
   }));
+  beforeEach(() => {
+    shellRedirectGuard = TestBed.get(ShellRedirectGuard);
+  });
+  fit('canActivate should always return false', () => {
+    expect(shellRedirectGuard.canActivate(new ActivatedRouteSnapshot(), routerSnapshot)).toBe(false);
+  });
 });
