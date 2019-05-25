93d2417d9c8d6642265f12616cb5173ab27690fa
dantethedevil <dr.likle@gmail.com>
2019-05-25 23:03:47 +0300

    Added unit tests for avatar component

--- src/app/admin-panel/dashboard/dashboard.component.spec.ts
+++ src/app/admin-panel/dashboard/dashboard.component.spec.ts
@@ -7,6 +7,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material.module';
 import { ChartsModule } from 'ng2-charts';
 import { RouterModule } from '@angular/router';
+import { streamData } from './mocks/stream-data';
 
 describe('DashboardComponent', () => {
   let component: DashboardComponent;
@@ -35,5 +36,15 @@ describe('DashboardComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('should update bar chart', () => {
+    (component as any).updateChart(streamData);
+    expect(component.studentsOfStream).toBe(23);
+    expect(component.classesStream).toBe(3);
+    expect(component.chartLabels.length).toBe(0);
+    for (let i = 0; i < streamData.studentsData.length; i++) {
+      expect(component.chartData[i].data[0]).toBe(streamData.studentsData[i].numOfStudents);
+      expect(component.chartData[i].label).toBe(streamData.studentsData[i].className);
+    }
   });
 });
