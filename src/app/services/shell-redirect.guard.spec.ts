733e98a0670fb55d4d98301e29e5cf082939aa35
trChris <christinagr123123@gmail.com>
2019-05-26 10:57:32 +0300

    Wrote unit tests for ShellRedirectGuard to check redirection

--- src/app/services/shell-redirect.guard.spec.ts
+++ src/app/services/shell-redirect.guard.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-import { RouterStateSnapshot, ActivatedRouteSnapshot } from '@angular/router';
+import { RouterStateSnapshot, ActivatedRouteSnapshot, Router } from '@angular/router';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ShellRedirectGuard } from './shell-redirect.guard';
+import { AuthService } from './auth.service';
 
 describe('Guard for Shell component which should redirect', () => {
   let shellRedirectGuard: ShellRedirectGuard;
+  let authService: AuthService;
   const routerSnapshot: any = jasmine.createSpyObj<RouterStateSnapshot>('RouterStateSnapshot', ['toString']);
+  const router = {navigate: jasmine.createSpy('navigate')};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,6 +19,8 @@ describe('Guard for Shell component which should redirect', () => {
       ],
       providers: [
         ShellRedirectGuard,
+        AuthService,
+        { provide: Router, useValue: router },
         { provide: RouterStateSnapshot, useValue: routerSnapshot }
       ]
     })
@@ -23,8 +28,24 @@ describe('Guard for Shell component which should redirect', () => {
   }));
   beforeEach(() => {
     shellRedirectGuard = TestBed.get(ShellRedirectGuard);
+    authService = TestBed.get(AuthService);
   });
   it('canActivate should always return false', () => {
     expect(shellRedirectGuard.canActivate(new ActivatedRouteSnapshot(), routerSnapshot)).toBe(false);
+  });
+  it('canActivate should redirect to /admin-panel/ if user is admin', () => {
+    spyOn(authService, 'getUserRole').and.returnValue('ROLE_ADMIN');
+    shellRedirectGuard.canActivate(new ActivatedRouteSnapshot(), routerSnapshot);
+    expect(router.navigate).toHaveBeenCalledWith(['/admin-panel/']);
+  });
+  it('canActivate should redirect to /journals/my-journals if user is teacher', () => {
+    spyOn(authService, 'getUserRole').and.returnValue('ROLE_TEACHER');
+    shellRedirectGuard.canActivate(new ActivatedRouteSnapshot(), routerSnapshot);
+    expect(router.navigate).toHaveBeenCalledWith(['journals', 'my-journals']);
+  });
+  it('canActivate should redirect to /student-book/ if user is student', () => {
+    spyOn(authService, 'getUserRole').and.returnValue('ROLE_USER');
+    shellRedirectGuard.canActivate(new ActivatedRouteSnapshot(), routerSnapshot);
+    expect(router.navigate).toHaveBeenCalledWith(['/student-book/']);
   });
 });
