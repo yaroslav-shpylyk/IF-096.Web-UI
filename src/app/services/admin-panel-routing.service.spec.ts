c31143f552b95e7f799b79489e781e7e58b3918f
trChris <christinagr123123@gmail.com>
2019-05-26 10:24:03 +0300

    Wrote unit tests for AdminPanelRoutingService for activeRoute method

--- src/app/services/admin-panel-routing.service.spec.ts
+++ src/app/services/admin-panel-routing.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AdminPanelRoutingService } from './admin-panel-routing.service';
+import { Router } from '@angular/router';
 
-describe('Service for managing selected section in admin panel menu', () => {
+describe('AdminPanelRoutingService checks truncateName method', () => {
   let adminPanelRoutingService: AdminPanelRoutingService;
 
   beforeEach(async(() => {
@@ -24,5 +25,67 @@ describe('Service for managing selected section in admin panel menu', () => {
   });
   it('should not truncate name if it is shorter than 2nd parameter', () => {
     expect(adminPanelRoutingService.truncateName('Khrystyna', 10)).toBe('Khrystyna');
+  });
+});
+
+describe('AdminPanelRoutingService checks activeRoute method for active urls from dropdown menu', () => {
+  let adminPanelRoutingService: AdminPanelRoutingService;
+  let router: Router;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        AdminPanelRoutingService,
+        {
+          provide: Router,
+          useValue: {
+            url: '/admin-panel/subjects'
+          }
+        }
+      ]
+    })
+      .compileComponents();
+  }));
+  beforeEach(() => {
+    adminPanelRoutingService = TestBed.get(AdminPanelRoutingService);
+    router = TestBed.get(Router);
+  });
+  it('should return correct path, icon, name, active class for /admin-panel/subjects path', () => {
+    expect(adminPanelRoutingService.activeRoute())
+      .toEqual({path: '/admin-panel/subjects', icon: 'library_books', name: 'Предмети', active: true});
+  });
+});
+
+describe('AdminPanelRoutingService checks activeRoute method for active urls from static menu', () => {
+  let adminPanelRoutingService: AdminPanelRoutingService;
+  let router: Router;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule
+      ],
+      providers: [
+        AdminPanelRoutingService,
+        {
+          provide: Router,
+          useValue: {
+            url: '/admin-panel'
+          }
+        }
+      ]
+    })
+      .compileComponents();
+  }));
+  beforeEach(() => {
+    adminPanelRoutingService = TestBed.get(AdminPanelRoutingService);
+    router = TestBed.get(Router);
+  });
+  it('should return correct default path, icon, name, active class for /admin-panel path', () => {
+    expect(adminPanelRoutingService.activeRoute())
+      .toEqual({path: '/admin-panel/groups', icon: 'group_work', name: 'Класи', active: false});
   });
 });
