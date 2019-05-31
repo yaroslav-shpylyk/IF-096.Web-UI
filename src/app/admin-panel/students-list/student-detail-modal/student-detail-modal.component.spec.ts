080cc223bb858eafefc6b77c6519cdec3f7996df
denys <den.hodovanets@gmail.com>
2019-05-31 23:40:32 +0300

    configure redirect test

--- src/app/admin-panel/students-list/student-detail-modal/student-detail-modal.component.spec.ts
+++ src/app/admin-panel/students-list/student-detail-modal/student-detail-modal.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed, inject } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { StudentDetailModalComponent, StudentDatailsComponent } from './student-detail-modal.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -11,37 +11,22 @@ import { of } from 'rxjs';
 import { NgModule } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
 import { StudentsService } from '../../../services/students.service';
+import { Observable, Observer } from 'rxjs';
+import { Student } from '../../../models/student';
 
 
 describe('StudentDetailModalComponent', () => {
-  let component: StudentDetailModalComponent;
-  let fixture: ComponentFixture<StudentDetailModalComponent>;
+  // let component: StudentDetailModalComponent;
+  // let fixture: ComponentFixture<StudentDetailModalComponent>;
 
-  let studentService: StudentsService;
-
-  let componentStudentService: StudentsService;
   const mockId = 13;
   const mockClassId = 23;
-  // let mockStudentService: any;
-  let studentServiceStub: Partial<StudentsService>;
-
 
 
   beforeEach(() => {
 
-    studentServiceStub = {
-      avatar: null,
-      classId: 52,
-      classe: '7-Б',
-      dateOfBirth: '2002-02-05',
-      email: '',
-      firstname: 'Артур',
-      id: 0,
-      lastname: 'Годованваі',
-      login: 'artur',
-      patronymic: 'Ігорович',
-      phone: '',
-    };
+    const routerSpy = jasmine.createSpyObj('Router', ['navigate']);
+    // const heroServiceSpy = jasmine.createSpyObj('HeroService', ['getHeroes']);
 
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule,
@@ -53,8 +38,8 @@ describe('StudentDetailModalComponent', () => {
       ],
       declarations: [StudentDetailModalComponent, AvatarComponent],
       providers: [
-        MatDialog, { provide: MatDialogRef, useValue: {} },
-        MatDialog, { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: {} },
+        { provide: MAT_DIALOG_DATA, useValue: { paramId: '2' } },
         {
           provide: ActivatedRoute,
           useValue: {
@@ -66,40 +51,140 @@ describe('StudentDetailModalComponent', () => {
             })
           }
         },
-        { provide: StudentsService, useValue: studentServiceStub }
+        { provide: Router, useValue: routerSpy }
+        // { provide: StudentsService, useValue: studentServiceStub }
       ],
     })
       .compileComponents();
 
 
+  });
+
+  describe(':', () => {
+    function setup() {
+      const fixture = TestBed.createComponent(StudentDetailModalComponent);
+      const component = fixture.debugElement.componentInstance;
+      const studentAsyncService = fixture.debugElement.injector.get(
+        StudentsService
+      );
+      const router = fixture.debugElement.injector.get(Router);
+
+      return { fixture, component, studentAsyncService, router };
+    }
+
+    it('should create', () => {
+      const { component } = setup();
 
 
-    fixture = TestBed.createComponent(StudentDetailModalComponent);
-    component = fixture.componentInstance;
+      expect(component).toBeTruthy();
+    });
 
-    studentService = fixture.debugElement.injector.get(StudentsService);
-    console.log('studentService', studentService);
 
-    componentStudentService = studentService;
-    // #docregion setup
-    // #docregion inject-from-testbed
-    // UserService from the root injector
-    studentService = TestBed.get(StudentsService);
-    // #enddocregion inject-from-testbed
+    it('should get mock studentService from injector', fakeAsync(() => {
 
-    fixture.detectChanges();
+      const { fixture, component, studentAsyncService } = setup();
+
+      const mockStudent = {
+        avatar: null,
+        classId: 52,
+        classe: '7-Б',
+        dateOfBirth: '2002-02-05',
+        email: '',
+        firstname: 'Артур',
+        id: 0,
+        lastname: 'Макар',
+        login: 'artur',
+        patronymic: 'Ігорович',
+        phone: '',
+      };
+
+
+
+      spyOn(studentAsyncService, 'getOneStudent').and.returnValue(
+        Observable.create((observer: Observer<Student>) => {
+          observer.next(mockStudent);
+          return observer;
+
+        }));
+
+
+      tick();
+
+      fixture.detectChanges();
+
+      console.log('data', component.data);
+      const name = fixture.debugElement.nativeElement.querySelector('mat-card-title');
+      const details = fixture.debugElement.nativeElement.querySelector('mat-card-content');
+
+
+
+      expect(name.children[0].textContent).toBe('Макар Артур');
+
+      expect(name.children[1].textContent).toBe('Ігорович');
+
+
+
+      console.log('22', component.classId);
+      expect(details.children[1].textContent).not.toBe('');
+
+
+
+
+    }));
+
+
+    //
+
+    it('should click editStudent will navigate', () => {
+
+      const { fixture, component, studentAsyncService, router } = setup();
+
+      const buttonEdit = fixture.nativeElement.querySelector('#btn');
+      console.log(buttonEdit);
+      fixture.detectChanges();
+      buttonEdit.click();
+      const call = spyOn(component, 'editStudent') as jasmine.Spy;
+
+      // component.editStudent();
+
+
+      // const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
+      const spy = router.navigate as jasmine.Spy;
+      // console.log('spy',spy);
+      const navArgs = spy.calls.first().args[0];
+
+      // const navigateSpy = spyOn(router, 'navigate');
+      console.dir(navArgs);
+      console.dir(['admin-panel', 'students', '2', 'edit']);
+
+      console.dir([navArgs] === ['admin-panel', 'students', '2', 'edit']);
+      expect(navArgs).toEqual(['admin-panel', 'students', '2', 'edit']);
+
+      // expect(component.editStudent).toHaveBeenCalled();
+
+
+
+      // component.editStudent();
+      // expect(navigateSpy).toHaveBeenCalledWith(['admin-panel', 'students', 23, 'edit']);
+
+
+      // expect(navArgs).toBe('/admin-panel/students/23/edit/',
+      //   'should nav to HeroDetail for first hero');
+    }
+    );
+
+    //
+
+
   });
 
 
-  it('TestBed and Component UserService should be the same', () => {
-    console.log('studentService', studentService);
-    expect(studentService === componentStudentService).toBe(true);
-  });
 
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+
+
+
 });
 
 @NgModule({
@@ -170,10 +255,9 @@ describe('StudentDatailsComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StudentDatailsComponent);
     component = fixture.componentInstance;
-    console.log('my', component);
+
     fixture.detectChanges();
-    console.log(fixture.nativeElement.querySelector('mat-card-title'));
-    fixture.detectChanges();
+
   });
 
 
@@ -187,7 +271,7 @@ describe('StudentDatailsComponent', () => {
     expect(component.paramId).toEqual(mockId);
   });
 
-  it('should ')
+
 
   //   it('openDialog should open StudentDetailModalComponent', done => {
   //     component.dialog
--- src/app/admin-panel/students-list/students-list.component.spec.ts
+++ src/app/admin-panel/students-list/students-list.component.spec.ts
@@ -1,16 +1,20 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, fakeAsync } from '@angular/core/testing';
 import { StudentsListComponent } from './students-list.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../../material.module';
-import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterTestingModule } from '@angular/router/testing';
+import { Student } from '../../models/student';
 import { AvatarComponent } from '../../shared/avatar/avatar.component';
+import { StudentsService } from '../../services/students.service';
+import { MatTableDataSource } from '@angular/material';
+import { Observable, Observer } from 'rxjs';
 
 describe('StudentsListComponent', () => {
-  let component: StudentsListComponent;
-  let fixture: ComponentFixture<StudentsListComponent>;
+  // let component: StudentsListComponent;
+  // let fixture: ComponentFixture<StudentsListComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -27,16 +31,87 @@ describe('StudentsListComponent', () => {
         AvatarComponent
       ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(StudentsListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  // beforeEach(() => {
+  //   function setup() {
+  //     const fixture = TestBed.createComponent(StudentsListComponent);
+  //     const component = fixture.componentInstance;
+  //     const userService = fixture.debugElement.injector.get(StudentsService);
+
+  //     return { fixture, component, userService };
+  //   }
+
+  // });
+
+
+
+
+  function setup() {
+    const fixture = TestBed.createComponent(StudentsListComponent);
+    const component = fixture.componentInstance;
+    const userService = fixture.debugElement.injector.get(StudentsService);
+
+    return { fixture, component, userService };
+  }
+
+
+
 
   it('should create', () => {
+    const { component } = setup();
     expect(component).toBeTruthy();
   });
+
+  it('should call delete student method', fakeAsync(() => {
+    const { component, fixture, userService } = setup();
+    const student = {
+      classId: 2,
+      firstname: 'Denys',
+      id: 1,
+      lastname: 'string',
+      login: 'string',
+      patronymic: 'string'
+     } as Student;
+    spyOn(userService, 'deleteStudent').and.returnValue(
+      Observable.create((observer: Observer<any>) => {
+        observer.complete();
+        return observer;
+    }));
+
+    const arrOfStudent = [ student ];
+    component.dataSource = new MatTableDataSource(arrOfStudent);
+
+    component.deleteStudent(1);
+    tick();
+    fixture.detectChanges();
+    expect(userService.deleteStudent).toHaveBeenCalledWith(1);
+  }));
+
+  it('should call delete student method 2', (() => {
+    const { component, fixture } = setup();
+    const student = {
+      classId: 2,
+      firstname: 'Denys',
+      id: 1,
+      lastname: 'string',
+      login: 'string',
+      patronymic: 'string'
+     } as Student;
+    const arrOfStudent = [ student ];
+    component.dataSource = new MatTableDataSource(arrOfStudent);
+
+
+    spyOn(component, 'deleteStudent').and.returnValue(() => {});
+    const button = fixture.nativeElement.querySelector('#deleteStudent');
+    button.click();
+    fixture.detectChanges();
+    expect(component.deleteStudent).toHaveBeenCalled();
+  }));
+
+
+
+
+
 });
