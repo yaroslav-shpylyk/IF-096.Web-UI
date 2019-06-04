63bbbd2182500c9275a59d02ab622b183ea70bfb
Tetiana Mysiuk <tivasyuk@i.ua>
2019-06-05 02:40:21 +0300

    added  pdf-doc generating with auto-table

--- src/app/admin-panel/schedule/pdf-preview/pdf-preview.component.ts
+++ src/app/admin-panel/schedule/pdf-preview/pdf-preview.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import * as jsPDF from 'jspdf';
+import 'jspdf-autotable';
 import { DialogData } from '../schedule.component';
 import { robotoFont } from '../../../../fonts/roboto.font';
 
@@ -11,7 +12,7 @@ import { robotoFont } from '../../../../fonts/roboto.font';
 })
 export class PdfPreviewComponent implements OnInit {
 
-  tableData = [new Array(6).fill(' ')];
+  tableData = [];
 
   constructor(
     public dialogRef: MatDialogRef<PdfPreviewComponent>,
@@ -20,32 +21,40 @@ export class PdfPreviewComponent implements OnInit {
 
   ngOnInit() {
     // generetes data for table-schedule
+    this.tableData.push(new Array(7).fill(' '));
+    this.tableData[0][0] = 1;
     for (let i = 0; i < this.data.weekDayName.length; i++) {
       const dailySchedul = this.data.dataSchedule[this.data.weekDayName[i].dailySubjectsName];
       for (let j = 0; j < dailySchedul.length; j++) {
+        let lessonSubjects = ' ';
         if (dailySchedul.value[j].firstGroup !== '') {
           if (this.tableData.length <= j) {
-            this.tableData.push(new Array(6).fill(' '));
+            this.tableData.push(new Array(7).fill(' '));
+            this.tableData[j][0] = j + 1;
           }
-          this.tableData[j][i] = dailySchedul.value[j];
-        } else if ('secondGroup' in dailySchedul.value[j]) {
+          lessonSubjects = dailySchedul.value[j].firstGroup.subjectName;
+        }
+        if ('secondGroup' in dailySchedul.value[j]) {
           if (dailySchedul.value[j].secondGroup !== '') {
             if (this.tableData.length <= j) {
-              this.tableData.push(new Array(6).fill(' '));
+              this.tableData.push(new Array(7).fill(' '));
+              this.tableData[j][0] = j + 1;
             }
-            this.tableData[j][i] = dailySchedul.value[j];
+            lessonSubjects = lessonSubjects +  '\r\n' +
+              dailySchedul.value[j].secondGroup.subjectName;
           }
         }
+        if (lessonSubjects !== ' ') {
+          this.tableData[j][i + 1] = lessonSubjects;
+        }
+
       }
     }
   }
 
   /** Method generates pdf-document from form data */
   onDownload() {
-    const paddingLeft = 24;
     const lineHeight = 20;
-    const cellHeight = 30;
-    const columnsCount = 6;
     let lineNumber = 2;
 
     const pdfDoc = new jsPDF('l', 'pt', 'a4');
@@ -67,46 +76,27 @@ export class PdfPreviewComponent implements OnInit {
 
     // generetes table headers
     lineNumber += 2;
-    const firstCellWidth = 20;
-    const cellWidth = (pdfDoc.internal.pageSize.width - paddingLeft * 2 - firstCellWidth) / columnsCount;
-    pdfDoc.cell(paddingLeft, lineHeight * lineNumber, firstCellWidth, lineHeight,
-      ' ', 0, 'center');
+    const tableHeader = [[' ']];
     for (const dayName of this.data.weekDayName) {
-      pdfDoc.cell(paddingLeft, lineHeight * lineNumber, cellWidth, lineHeight,
-        dayName.legendDay, 0, 'center');
+      tableHeader[0].push(dayName.legendDay);
     }
-
-    // generetes table data
-    pdfDoc.setFontSize(10);
-    lineNumber += 1;
-    let row = 1;
-    for (const lesson of this.tableData) {
-      pdfDoc.cell(paddingLeft, lineHeight * lineNumber, firstCellWidth, cellHeight,
-        String(row), row, 'center');
-      for (const subject of lesson) {
-        let cellText = '';
-        if (subject === ' ') {
-          cellText = subject;
-        } else {
-          if ('secondGroup' in subject) {
-            cellText = '';
-            if (subject.firstGroup !== '') {
-              cellText = subject.firstGroup.subjectName;
-            }
-            if (subject.secondGroup !== '') {
-              cellText =  cellText + '\r\n' + subject.secondGroup.subjectName;
-            }
-          } else {
-            cellText = subject.firstGroup.subjectName;
-          }
-        }
-        pdfDoc.cell(paddingLeft, lineHeight * lineNumber, cellWidth, cellHeight,
-          cellText, row, 'center');
-      }
-      row += 1;
-      lineNumber += 1;
-    }
-
+    // generetes table
+    pdfDoc.autoTable({
+      startY: lineHeight * lineNumber,
+      head: tableHeader,
+      headStyles: {
+        fontStyle: 'Roboto-Regular',
+        halign: 'center',
+        fontSize: 12,
+        fillColor: [78, 125, 185]
+      },
+      body: this.tableData,
+      styles: {
+        font: 'Roboto-Regular',
+        fontSize: 10
+      },
+      theme: 'grid'
+    });
     const fileName = `schedule${this.data.selectedClass}.pdf`;
     pdfDoc.save(fileName);
   }
