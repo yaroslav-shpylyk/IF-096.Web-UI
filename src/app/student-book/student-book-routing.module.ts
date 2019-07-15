849756cffc1f6d0eb45c8fe8c24b77331bcfd20d
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-07-15 13:06:31 +0300

    added route for scores

--- src/app/student-book/student-book-routing.module.ts
+++ src/app/student-book/student-book-routing.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { ProgressComponent } from '../progress/progress/progress.component';
 import { StudentBookComponent } from './student-book/student-book.component';
-
+import { ScoresReportComponent } from './scores-report/scores-report.component';
 const routes: Routes = [
   {
     path: '',
     component: StudentBookComponent
+  },
+  {
+    path: 'scores',
+    component: ScoresReportComponent
   }
 ];
 
