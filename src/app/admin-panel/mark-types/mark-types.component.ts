6500ff5149c2c0786350c0435165f640b8731ee5
Volodymyr Skrypnyk <volodimir.skripnik@gmail.com>
2019-08-05 09:56:56 +0300

    added display of table with mark types

--- src/app/admin-panel/mark-types/mark-types.component.ts
+++ src/app/admin-panel/mark-types/mark-types.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, HostListener } from '@angular/core';
+import { MarkTypesService } from '../../services/mark-types.service';
+import { MarkType } from '../../models/mark-type';
+import { MatTableDataSource, MatSort } from '@angular/material';
+import { MatDialog } from '@angular/material';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-mark-types',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MarkTypesComponent implements OnInit {
 
-  constructor() { }
+  constructor(public markTypeService: MarkTypesService, public dialog: MatDialog) { }
+
+  allMarkTypes: MarkType[];
+  public displayedColumns: string[] = ['markType', 'markDescription', 'edit'];
+  public dataSource: MatTableDataSource<MarkType>;
+  @ViewChild(MatSort) sort: MatSort;
+  prevScrollpos = window.pageYOffset;
+  scroll = 'scrollTop';
+  marks: Observable<MarkType[]>;
+  @HostListener('window:scroll', [])
 
   ngOnInit() {
+    this.getMarkTypes();
   }
 
+  /**
+   * The method gets all types of marks and assigns them into data for a table view
+   */
+
+  getMarkTypes() {
+    this.markTypeService.getAllMarkTypes().subscribe(
+      markTypes => {
+        console.log(markTypes);
+        this.allMarkTypes = markTypes;
+        this.dataSource = new MatTableDataSource(this.allMarkTypes);
+        this.dataSource.sort = this.sort;
+      }
+    );
+  }
+
+  /**
+   * Method open a dialog window, send data to the dialog window and update the list of mark types
+   * @param markType - data which sends to the dialog window
+   */
+  openDialog(markType: MarkType) {
+
+  }
+
+  /**
+   * Method which filter value in table,removes the leading
+   * and trailing white space and line terminator characters from a string.
+   * @param filterValue - input value for which is filtered
+   */
+  applyFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  onScrollEvent() {
+    const currentScrollPos = window.pageYOffset;
+    if (this.prevScrollpos > currentScrollPos) {
+      this.scroll = 'scrollTop';
+    } else {
+      this.scroll = 'scrollBottom';
+    }
+    this.prevScrollpos = currentScrollPos;
+  }
 }
--- src/app/services/mark-types.service.ts
+++ src/app/services/mark-types.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { MarkType } from '../models/mark-type';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root'
 })
 export class MarkTypesService {
 
-  constructor() { }
+  constructor( private http: HttpClient) { }
+
+  /**
+   * Method gets all mark types
+   * @param options - Object of params, which we use as query with request
+   * @returns - array of types of marks
+   */
+
+   public getAllMarkTypes(): Observable<MarkType[]> {
+    return this.http.get<{status: any, data: MarkType[]}>(`/mark_types`)
+    .pipe(
+      map(result => result.data)
+    );
+   }
 }
